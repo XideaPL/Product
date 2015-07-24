7a8331cec7a9ced91c73e775387887c539924060
Artur Pszczółka <artur.pszczolka@xidea.pl>
2015-07-24 11:23:46 +0200

    Updated component for pagination support

--- Loader/ProductLoaderInterface.php
+++ Loader/ProductLoaderInterface.php
@@ -24,4 +24,14 @@ interface ProductLoaderInterface extends ModelLoaderInterface
      * @return \Xidea\Component\Product\Model\ProductInterface
      */
     function load($id);
+    
+    /**
+     * Returns a set of objects.
+     * 
+     * @param int $page
+     * @param int $limit
+     * 
+     * @return array
+     */
+    function loadByPage($page = 1, $limit = 25);
 }
